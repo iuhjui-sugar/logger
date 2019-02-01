505a461e63437e855453c770709d09d9c07a335b
iuhjui <1332852718@qq.com>
2019-02-01 15:05:09 +0800

    dd

--- define/entity.go
+++ define/entity.go
@@ -1,72 +1,69 @@
-package define;
+package define
 
 import (
-    "time"
+	"time"
 
-    "github.com/keystone-coin/logger/dot"
+	"github.com/keystone-coin/logger/dot"
 )
 
-
-type IEntity interface{
-    GetLevel()Level
-    SetLevel(Level)
-    GetTimestamp()uint64
-    SetTimestamp(uint64)
-    SetTemplate(string)
-    GetTemplate()string
-    Derive(map[string]string) IEntity
-    String()string
+type IEntity interface {
+	GetLevel() Level
+	SetLevel(Level)
+	GetTimestamp() uint64
+	SetTimestamp(uint64)
+	SetTemplate(string)
+	GetTemplate() string
+	SetData(map[string]string)
+	String() string
 }
 
 type Entity struct {
-    level     Level
-    timestamp uint64
-    template  string
-    data      map[string]string
+	level     Level
+	timestamp uint64
+	template  string
+	data      map[string]string
 }
 
 func NewEntity(level Level) *Entity {
-    entity := &Entity{};
-    entity.level = level;
-    entity.timestamp = uint64(time.Now().Unix());
-    return entity;
+	entity := &Entity{}
+	entity.level = level
+	entity.timestamp = uint64(time.Now().Unix())
+	return entity
 }
 
-func (e *Entity) GetLevel() Level{
-    return e.level;
+func (e *Entity) GetLevel() Level {
+	return e.level
 }
 
-func (e *Entity) SetLevel(l Level){
-    e.level = l;
-    return;
+func (e *Entity) SetLevel(l Level) {
+	e.level = l
+	return
 }
 
 func (e *Entity) GetTimestamp() uint64 {
-    return e.timestamp;
+	return e.timestamp
 }
 
 func (e *Entity) SetTimestamp(timestamp uint64) {
-    e.timestamp = timestamp;
-    return;
+	e.timestamp = timestamp
+	return
 }
 
-func (e *Entity) GetTemplate() string{
-    return e.template;
+func (e *Entity) GetTemplate() string {
+	return e.template
 }
 
 func (e *Entity) SetTemplate(template string) {
-    e.template = template;
-    return;
+	e.template = template
+	return
 }
 
-func (e *Entity) Derive(data map[string]string) IEntity {
-    derive := NewEntity(e.level);
-    derive.SetTemplate(e.template);
-    derive.data = data;
-    return derive;
+func (e *Entity) SetData(data map[string]string) {
+	e.data = data
+	return
 }
 
-func (e *Entity) String() string  {
-    str,_:= dot.New(e.template).Complie(e.data);
-    return str;
+func (e *Entity) String() string {
+	str, _ := dot.New(e.template).Complie(e.data)
+	return str
 }
--- logger.go
+++ logger.go
@@ -1,59 +1,44 @@
-package logger;
+package logger
 
 import (
-    "sync"
+	"sync"
 
-    "github.com/keystone-coin/logger/define"
-    "github.com/keystone-coin/logger/handler"
-    "github.com/keystone-coin/logger/format"
+	"github.com/keystone-coin/logger/define"
+	"github.com/keystone-coin/logger/format"
+	"github.com/keystone-coin/logger/handler"
 )
 
-
 type Logger struct {
-    handler *handler.SwapHandler
-    entitys map[string]define.IEntity
-    mu    sync.Mutex
+	handler *handler.SwapHandler
+	mu      sync.Mutex
 }
 
 func NewLogger() *Logger {
-    l := &Logger{}
-    l.makeHandler();
-    l.makeEntitys();
-    return l;
+	l := &Logger{}
+	l.makeHandler()
+	return l
 }
 
-func (l *Logger) makeHandler(){
-    f    := format.NewTerminalFormat();
-    std  := handler.NewStdoutHandler(f);
-    swap := handler.NewSwapHandler(std);
-    l.handler = swap;
-    return;
-}
-
-func (l *Logger) makeEntitys() {
-    l.entitys = map[string]define.IEntity{};
-    return;
+func (l *Logger) makeHandler() {
+	f := format.NewTerminalFormat()
+	std := handler.NewStdoutHandler(f)
+	swap := handler.NewSwapHandler(std)
+	l.handler = swap
+	return
 }
 
 func (l *Logger) write(ie define.IEntity) {
-    l.handler.Log(ie);
+	l.handler.Log(ie)
 }
 
 func (l *Logger) SetHandler(other define.Handler) {
 	l.handler.Swap(other)
 }
 
-func (l *Logger) Bind(name string,ie define.IEntity){
-    l.mu.Lock()
-    defer l.mu.Unlock();
-    l.entitys[name] = ie;
-}
-
-func (l *Logger) Emit(name string,data map[string]string){
-    ie,exists := l.entitys[name];
-    if exists == false {
-        return
-    }
-    l.write(ie.Derive(data));
-    return;
+func (l *Logger) Infof(template string, data map[string]string) {
+	entity := define.NewEntity(define.INFO)
+	entity.SetTemplate(template)
+	entity.SetData(data)
+	l.write(entity)
+	return
 }
